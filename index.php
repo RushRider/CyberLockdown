2c6a0829c35f5472fe994e6dbdd3c47a31b4b574
「Rush」 <tcautenet@myges.fr>
2024-03-25 17:59:32 +0100

    qgsuvcgsquv

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-require_once("controller\controller.php"); 
+require_once("controller/controller.php"); 
 footerDisplay();
     if(htmlspecialchars_decode(isset($_POST["p"])) && htmlspecialchars_decode(!empty($_POST["p"]))){
         $p = htmlspecialchars_decode($_POST["p"]);
