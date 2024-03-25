f93c1ad157d880e9f9b1b9ad034163b1482b9953
「Rush」 <tcautenet@myges.fr>
2024-03-25 17:02:44 +0100

    MVC fix2

--- CyberLockdown/index.php
+++ CyberLockdown/index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CyberLockdown</title>
-    <link href="style.css" rel="stylesheet">
-</head>
-<body>
 <?php 
 require_once("controller\controller.php"); 
+require_once("views\footer.php");
     if(htmlspecialchars_decode(isset($_POST["p"])) && htmlspecialchars_decode(!empty($_POST["p"]))){
         $p = htmlspecialchars_decode($_POST["p"]);
         if($p=="connect"){
@@ -22,9 +14,3 @@ require_once("controller\controller.php");
         connectDisplay();
         echo"<link href='assets/connect.css' rel='stylesheet'>";
     }
-?>    
-    <footer>
-        <p>Copyright © 2024 CyberLockdown Inc. All rights reserved. Terms of services | Contact | Privacy policies</p>
-    </footer>
-</body>
-</html>
--- CyberLockdown/views/connect.php
+++ CyberLockdown/views/connect.php
@@ -1,24 +1,34 @@
-<div class="logo">
-    <img src="logo.png">
-</div>
-<div class="cbld_titre">
-    <p>CyberLockdown</p>
-</div>
-<div class="fenetre_princ">
-<form action="accueil.html" method="post" class="form_id">
-    <div class="id_fen">
-        <label for="id">Identifiant</label>
-        <input type="text" name="name" id="name" required />
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CyberLockdown</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="logo">
+        <img src="logo.png">
     </div>
-    <div class="mdp_fen">
-        <label for="id">Mot de passe</label>
-        <input type="password" name="mdp" id="mdp" required />
+    <div class="cbld_titre">
+        <p>CyberLockdown</p>
     </div>
-    <div class="mdp_oubli">
-        <p>Mot de passe oublié ?</p>
+    <div class="fenetre_princ">
+    <form action="accueil.html" method="post" class="form_id">
+        <div class="id_fen">
+            <label for="id">Identifiant</label>
+            <input type="text" name="name" id="name" required />
+        </div>
+        <div class="mdp_fen">
+            <label for="id">Mot de passe</label>
+            <input type="password" name="mdp" id="mdp" required />
+        </div>
+        <div class="mdp_oubli">
+            <p>Mot de passe oublié ?</p>
+        </div>
+        <div class="submt_fen">
+            <input type="submit" value="Se connecter">
+        </div>
+    </form>
     </div>
-    <div class="submt_fen">
-        <input type="submit" value="Se connecter">
-    </div>
-</form>
-</div>
+</body>
