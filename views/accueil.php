815e79efec762afabaa6035fa29c4372de679344
「Rush」 <tcautenet@myges.fr>
2024-03-25 18:28:31 +0100

    fix acceuil.php

--- views/accueil.php
+++ views/accueil.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vos mots de passe</title>
-    <link href="assets/home.css" rel="stylesheet">
+    <link href="../assets/home.css" rel="stylesheet">
 </head>
 <header>
-    <img src="assets\logo.png">
+    <img src="../assets/logo.png">
     <div class="theader">Vos mots de passe</div>
     <div class="profile">Pseudo</div>
-    <img src="assets\icon_avatar.png">
+    <img src="../assets/icon_avatar.png">
 </header>
 
 <body>
