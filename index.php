211ad7734705afbe781f43f6cdf751ed794dcffc
「Rush」 <tcautenet@myges.fr>
2024-03-27 19:59:17 +0100

    creation e compte

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php 
 require_once("controller/controller.php"); 
 footerDisplay();
-    if(htmlspecialchars_decode(isset($_POST["p"])) && htmlspecialchars_decode(!empty($_POST["p"]))){
-        $p = htmlspecialchars_decode($_POST["p"]);
-        if($p=="connect"){
-            connectDisplay();
-        }else if($p=="home"){
-            homeDisplay();
-        }
-    }else{
+    if(htmlspecialchars(isset($_POST["mail"])) && htmlspecialchars(!empty($_POST["mail"])) && htmlspecialchars(!empty($_POST["mdp1"]))==htmlspecialchars(!empty($_POST["mdp2"]))){
+        $mail = htmlspecialchars($_POST["mail"]);
+        $pseudo = htmlspecialchars($_POST["pseudo"]);
+        $nom = htmlspecialchars($_POST["nom"]);
+        $prenom = htmlspecialchars($_POST["prenom"]);
+        $mdp = htmlspecialchars($_POST["mdp"]);
+        $num = htmlspecialchars($_POST["num"]);
+        $pays = htmlspecialchars($_POST["pays"]);
+        $sel=openssl_random_pseudo_bytes(16);
+        $sel_hex = bin2hex($sel);
+        $hash=password_hash($mdp.$sel,PASSWORD_DEFAULT);
+        newAccount($pseudo,$hash,$nom,$prenom,$mail,$num,$pays);
         connectDisplay();
+    }else{
+        echo"<h1>sale_merde</h1>";
     }
