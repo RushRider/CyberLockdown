9b3ae6f9d2aebe4c05f2f8452fde070db6100df5
「Rush」 <tcautenet@myges.fr>
2024-03-27 20:21:11 +0100

    tkt2

---
+++ views/connect.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CyberLockdown</title>
+    <link href="/assets/connect.css" rel="stylesheet">
+</head>
+<body>
+    <div class="logo">
+        <img src="assets/logo.png">
+    </div>
+    <div class="cbld_titre">
+        <p>CyberLockdown</p>
+    </div>
+    <div class="fenetre_princ">
+    <form action="views/home.php" method="post" class="form_id">
+        <div class="id_fen">
+            <label for="id">Identifiant</label>
+            <input type="text" name="name" id="name" required />
+        </div>
+        <div class="mdp_fen">
+            <label for="id">Mot de passe</label>
+            <input type="password" name="mdp" id="mdp" required />
+        </div>
+        <div class="mdp_oubli">
+            <p>Mot de passe oublié ?</p>
+        </div>
+        <div class="submt_fen">
+            <input type="submit" value="Se connecter">
+        </div>
+        <div>
+            <a href="views/register.php">Pas de compte ?</a>
+        </div>
+    </form>
+    </div>
+</body>
